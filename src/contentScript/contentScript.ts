ec96329802ee6e5fc95d78151bd8e8c1f03a6528
rohitkumarm <rohitkumarrk13568@gmail.com>
2022-05-01 13:08:51 +0530

    added logic to block the ads for nytimes site

--- src/contentScript/contentScript.ts
+++ src/contentScript/contentScript.ts
@@ -1,1 +1,24 @@
-// TODO: content script
+const rules: {
+  [url: string]: () => void
+} = {
+  'https://www.nytimes.com/section/technology': filterNYTTechnology2,
+}
+
+function filterNYTTechnology() {
+  const app = document.getElementById('site-content')
+  const wrapper = document.getElementById('top-wrapper')
+  app.removeChild(wrapper)
+}
+
+function filterNYTTechnology2() {
+  const divs = document.getElementsByTagName('div')
+  for (const div of divs) {
+    if (div.className.indexOf('ad') != -1) {
+      div.style.display = 'none'
+    }
+  }
+}
+
+if (document.URL in rules) {
+  rules[document.URL]()
+}
